8a51452c84fbd1394daba883ca6ec1c368795ea3
renegadespy256 <bhattacharjeesoham5@gmail.com>
2021-06-07 22:13:00 +0530

    added-controller

---
+++ src/test/java/controller/JokeController.java
@@ -0,0 +1,30 @@
+package controller;
+
+import guru.springframework.spring5jokesappv2.services.JokeService;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private final JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/",""})
+    public String showJokes(Model model){
+
+        model.addAttribute("joke",jokeService.getJoke());
+
+        return "index";
+
+    }
+
+
+}
