f49c53ffab68a187563dcced3cf01d0ecb3e7442
renegadespy256 <bhattacharjeesoham5@gmail.com>
2021-06-07 22:21:05 +0530

    fixed-error 1.1

--- src/main/java/guru/springframework/spring5jokesappv2/controller/JokeController.java
+++ src/main/java/guru/springframework/spring5jokesappv2/controller/JokeController.java
@@ -1,6 +1,6 @@
 package guru.springframework.spring5jokesappv2.controller;
 
-import services.JokeService;
+import guru.springframework.spring5jokesappv2.services.JokeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
